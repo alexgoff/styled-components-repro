7a38f7962ad46743a989c73c91ea848d95b6b0d4
Alexandra Goff <agoff@lsst.org>
2023-05-15 14:28:08 -0700

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
+import GlobalStyle from "../lib/styled";
+import StyledComponentsRegistry from "../lib/registry";
+
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head />
-      <body>{children}</body>
+      <head>
+        <title>Demo Page</title>
+      </head>
+      <body>
+        <StyledComponentsRegistry>
+          <GlobalStyle />
+          {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <section>
+      <h1>Next.js 13.4</h1>
+      <p>Next.js demo page using styled-components global styles.</p>
+    </section>
+  );
 }
