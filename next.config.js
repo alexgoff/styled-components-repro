a15a895d4bfc569046d6072969cca16212532702
Alexandra Goff <agoff@lsst.org>
2023-05-16 09:25:03 -0700

    [C] add babel config and next.config ssr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: { ssr: true },
+  },
   experimental: {
     appDir: true,
   },
-}
+};
